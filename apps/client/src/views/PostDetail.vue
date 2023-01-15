62e7aba188073d72d913a710af976b8971ec3f0b
mohammad mirfatemi <mirfatemi7@gmail.com>
2023-01-15 19:13:38 +0330

    ...

--- apps/client/src/views/PostDetail.vue
+++ apps/client/src/views/PostDetail.vue
@@ -1,6 +1,5 @@
 <template>
-    <div>{{ $route.params.id }}</div>
-    <pre>{{ data }}</pre>
+    <main></main>
 </template>
 
 <script setup>
@@ -8,7 +7,6 @@ import { useRoute } from 'vue-router'
 import { usePostDetail } from '../Queries'
 const route = useRoute()
 const { data } = usePostDetail(route.params.id)
-console.log(data)
 </script>
 
 <style scoped>
